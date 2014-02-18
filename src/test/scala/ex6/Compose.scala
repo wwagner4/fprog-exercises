06fb7c32830586372d9ff3449a9dfd7d77f7097a
wwagner4 <wwagner4@gmail.com>
2014-02-18 18:09:37 +0100

    Compose new test text

--- src/test/scala/ex6/Compose.scala
+++ src/test/scala/ex6/Compose.scala
@@ -7,14 +7,14 @@ class Compose extends FeatureSpec with GivenWhenThen {
 
   def compose[A, B, C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
 
-  feature("Concatenate") {
-    scenario("Concatenate simple math functions") {
+  feature("Function composition") {
+    scenario("Compose simple math functions") {
 
-      Given("two math functions")
+      Given("two math functions add5 mul10")
       def add5(a: Int): Int = a + 5
       def mul10(a: Int): Int = a * 10
 
-      When("composing add5 and mul10")
+      When("composing add5 and mul10 to a new function f")
       val f = compose(mul10, add5)
 
       Then("f(5) should result in 100")
