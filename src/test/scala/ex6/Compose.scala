c6f55f0c051b22c366d27e492f4511fdf97b3c35
wwan <wwagner4@gmail.com>
2014-02-18 14:47:30 +0100

    added compose. not yet working

---
+++ src/test/scala/ex6/Compose.scala
@@ -0,0 +1,25 @@
+package ex6
+
+import org.scalatest.GivenWhenThen
+import org.scalatest.FeatureSpec
+
+class Compose extends FeatureSpec with GivenWhenThen {
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = ???
+
+  feature("Concatenate") {
+    scenario("Concatenate simple math functions") {
+
+      Given("two math functions")
+      def add5(a: Int): Int = a + 5
+      def mul10(a: Int): Int = a * 10
+
+      When("composing add5 and mul10")
+      val f = compose(add5, mul10)
+
+      Then("f(5) should result in 100")
+      assert(f(5) === 100)
+    }
+  }
+
+}
