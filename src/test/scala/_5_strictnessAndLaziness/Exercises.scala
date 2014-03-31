dfb26ea95e60a16c3f63bbaaa8ad318ad2207cc7
wwagner4 <wwagner4@gmail.com>
2014-03-31 19:23:22 +0200

    ex 1 and 2 finished

--- src/test/scala/_5_strictnessAndLaziness/Exercises.scala
+++ src/test/scala/_5_strictnessAndLaziness/Exercises.scala
@@ -3,22 +3,38 @@ package _5_strictnessAndLaziness
 import org.scalatest.FunSuite
 
 class Exercises extends FunSuite {
+  
+  case class Cons[+A](head: A, tail: Stream[A])
 
   trait Stream[+A] {
-    def uncons: Option[(A, Stream[A])]
+    def uncons: Option[Cons[A]]
     def isEmpty: Boolean = uncons.isEmpty
 
     // Exercise 1: convert a stream to a list.
-    def toList[A]: List[A] = {
-      def _toList(s: Option[(A, Stream[A])]): List[A] = {
-        if (s.isEmpty) Nil
-        else s.get._1 :: _toList(s.get._2.uncons)
+    def toList: List[A] = {
+      def tl(uc: Option[Cons[A]]): List[A] = {
+        uc match {
+          case None => Nil
+          case Some(c) => c.head :: tl(c.tail.uncons)
+        }
       }
       if (isEmpty) Nil
-      else ???
+      else tl(uncons)
+    }
+    
+    def take(n: Int): Stream[A] = {
+      def tl(n: Int, uc: Stream[A]): Stream[A] = {
+        if (n <= 0) Stream.empty
+        else if (uc.isEmpty) Stream.empty
+        else uc.uncons match {
+          case None => Stream.empty
+          case Some(c) => Stream.cons(c.head, tl(n -1, c.tail))
+        }
+      }
+      if (isEmpty) Stream.empty
+      else tl(n, this)
     }
   }
-
   object Stream {
 
     def empty[A]: Stream[A] =
@@ -28,7 +44,7 @@ class Exercises extends FunSuite {
 
     def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
       new Stream[A] {
-        lazy val uncons = Some((hd, tl))
+        lazy val uncons = Some(Cons(hd, tl))
       }
 
     def apply[A](as: A*): Stream[A] =
@@ -37,12 +53,54 @@ class Exercises extends FunSuite {
 
   }
 
-  test("Exercise 1") {
+  test("Exercise 1 toList") {
     import Stream._
 
-    val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
-    val string = s.toList.mkString(",")
-    assert("1, 2, 3" === string)
+    {
+      val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+      val l = s.toList
+      assert(List(1,2,3) === l)
+    }
+    {
+      val s: Stream[Int] = cons(3, empty)
+      val l = s.toList
+      assert(List(3) === l)
+    }
+    {
+      val s: Stream[Int] = empty
+      val l = s.toList
+      assert(Nil === l)
+    }
+  }
+
+  test("Exercise 2 take") {
+    import Stream._
+
+    {
+      val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+      val l = s.take(1).toList
+      assert(List(1) === l)
+    }
+    {
+      val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+      val l = s.take(5).toList
+      assert(List(1, 2, 3) === l)
+    }
+    {
+      val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+      val l = s.take(0).toList
+      assert(Nil === l)
+    }
+    {
+      val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+      val l = s.take(-1).toList
+      assert(Nil === l)
+    }
+    {
+      val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+      val l = s.take(-100).toList
+      assert(Nil === l)
+    }
   }
 
 }
