1bcff3ae2e52448a24de89b09dd21bba30dcba39
wwan <wwagner4@gmail.com>
2014-04-01 19:44:47 +0200

    removed impl from test

---
+++ src/main/scala/fprog/Stream.scala
@@ -0,0 +1,92 @@
+package fprog
+
+  object Stream {
+
+    case class Cons[+A](head: A, tail: Stream[A])
+
+    def empty[A]: Stream[A] =
+      new Stream[A] {
+        def uncons = None
+      }
+
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
+      new Stream[A] {
+        lazy val uncons = Some(Cons(hd, tl))
+      }
+
+    def apply[A](as: A*): Stream[A] =
+      if (as.isEmpty) empty
+      else cons(as.head, apply(as.tail: _*))
+
+  }
+
+
+  trait Stream[+A] {
+    import Stream._
+    
+    def uncons: Option[Cons[A]]
+    def isEmpty: Boolean = uncons.isEmpty
+
+    // Exercise 1: convert a stream to a list.
+    def toList: List[A] = {
+      def _toList(uc: Option[Cons[A]]): List[A] = {
+        uc match {
+          case None => Nil
+          case Some(c) => c.head :: _toList(c.tail.uncons)
+        }
+      }
+      if (isEmpty) Nil
+      else _toList(uncons)
+    }
+
+    def take(n: Int): Stream[A] = {
+      def _take(n: Int, uc: Stream[A]): Stream[A] = {
+        if (n <= 0) Stream.empty
+        else if (uc.isEmpty) Stream.empty
+        else uc.uncons match {
+          case None => Stream.empty
+          case Some(c) => Stream.cons(c.head, _take(n - 1, c.tail))
+        }
+      }
+      if (isEmpty) Stream.empty
+      else _take(n, this)
+    }
+    def takeWhile(f: A => Boolean): Stream[A] = {
+      def _takeWhile(uc: Stream[A]): Stream[A] = {
+        if (uc.isEmpty) Stream.empty
+        else uc.uncons match {
+          case None => Stream.empty
+          case Some(c) => {
+            if (f(c.head)) Stream.cons(c.head, _takeWhile(c.tail))
+            else Stream.empty
+          }
+        }
+      }
+      if (isEmpty) Stream.empty
+      else _takeWhile(this)
+    }
+    // Implement forAll, which checks that all elements in the
+    // match a given predicate. Your implementation should Stream terminate the
+    // traversal as soon as it encounters a non-matching value.
+    def forAll(p: A => Boolean): Boolean = {
+      def _forAll(uc: Stream[A]): Boolean = {
+        if (uc.isEmpty) true
+        else uc.uncons match {
+          case None => true
+          case Some(c) => {
+            if (p(c.head)) _forAll(c.tail)
+            else false
+          }
+        }
+      }
+      if (isEmpty) true
+      else _forAll(this)
+    }
+
+    def foldRight[B](z: => B)(f: (A, => B) => B): B =
+      uncons match {
+        case Some(Cons(head, tail)) => f(head, tail.foldRight(z)(f))
+        case None => z
+      }
+  }
+  
--- src/test/scala/_5_strictnessAndLaziness/Exercises.scala
+++ src/test/scala/_5_strictnessAndLaziness/Exercises.scala
@@ -3,6 +3,8 @@ package _5_strictnessAndLaziness
 import org.scalatest.FunSuite
 import org.scalatest.Matchers._
 
+import fprog._
+
 class Exercises extends FunSuite {
 
   // Chattering operators 
@@ -13,92 +15,6 @@ class Exercises extends FunSuite {
   implicit class TestDouble(i: Double) {
     def /#(b: Double) = { println(s"# $i/$b"); i / b }
     def +#(b: Double) = { println(s"# $i+$b"); i + b }
-  }
-
-  case class Cons[+A](head: A, tail: Stream[A])
-
-  trait Stream[+A] {
-    def uncons: Option[Cons[A]]
-    def isEmpty: Boolean = uncons.isEmpty
-
-    // Exercise 1: convert a stream to a list.
-    def toList: List[A] = {
-      def _toList(uc: Option[Cons[A]]): List[A] = {
-        uc match {
-          case None => Nil
-          case Some(c) => c.head :: _toList(c.tail.uncons)
-        }
-      }
-      if (isEmpty) Nil
-      else _toList(uncons)
-    }
-
-    def take(n: Int): Stream[A] = {
-      def _take(n: Int, uc: Stream[A]): Stream[A] = {
-        if (n <= 0) Stream.empty
-        else if (uc.isEmpty) Stream.empty
-        else uc.uncons match {
-          case None => Stream.empty
-          case Some(c) => Stream.cons(c.head, _take(n - 1, c.tail))
-        }
-      }
-      if (isEmpty) Stream.empty
-      else _take(n, this)
-    }
-    def takeWhile(f: A => Boolean): Stream[A] = {
-      def _takeWhile(uc: Stream[A]): Stream[A] = {
-        if (uc.isEmpty) Stream.empty
-        else uc.uncons match {
-          case None => Stream.empty
-          case Some(c) => {
-            if (f(c.head)) Stream.cons(c.head, _takeWhile(c.tail))
-            else Stream.empty
-          }
-        }
-      }
-      if (isEmpty) Stream.empty
-      else _takeWhile(this)
-    }
-    // Implement forAll, which checks that all elements in the
-    // match a given predicate. Your implementation should Stream terminate the
-    // traversal as soon as it encounters a non-matching value.
-    def forAll(p: A => Boolean): Boolean = {
-      def _forAll(uc: Stream[A]): Boolean = {
-        if (uc.isEmpty) true
-        else uc.uncons match {
-          case None => true
-          case Some(c) => {
-            if (p(c.head)) _forAll(c.tail)
-            else false
-          }
-        }
-      }
-      if (isEmpty) true
-      else _forAll(this)
-    }
-
-    def foldRight[B](z: => B)(f: (A, => B) => B): B =
-      uncons match {
-        case Some(Cons(head, tail)) => f(head, tail.foldRight(z)(f))
-        case None => z
-      }
-  }
-  object Stream {
-
-    def empty[A]: Stream[A] =
-      new Stream[A] {
-        def uncons = None
-      }
-
-    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
-      new Stream[A] {
-        lazy val uncons = Some(Cons(hd, tl))
-      }
-
-    def apply[A](as: A*): Stream[A] =
-      if (as.isEmpty) empty
-      else cons(as.head, apply(as.tail: _*))
-
   }
 
   test("Exercise 1 toList") {
@@ -225,6 +141,7 @@ class Exercises extends FunSuite {
       re should be(0.166666 +- 0.01)
     }
   }
+  
   test("Exercise 5 forAll") {
     import Stream._
     {
