046259db11bed87187a80b35e31e4fd6b010a28a
wwan <wwagner4@gmail.com>
2014-04-01 18:44:14 +0200

    for all working

--- src/test/scala/_5_strictnessAndLaziness/Exercises.scala
+++ src/test/scala/_5_strictnessAndLaziness/Exercises.scala
@@ -3,7 +3,7 @@ package _5_strictnessAndLaziness
 import org.scalatest.FunSuite
 
 class Exercises extends FunSuite {
-  
+
   case class Cons[+A](head: A, tail: Stream[A])
 
   trait Stream[+A] {
@@ -12,41 +12,58 @@ class Exercises extends FunSuite {
 
     // Exercise 1: convert a stream to a list.
     def toList: List[A] = {
-      def tl(uc: Option[Cons[A]]): List[A] = {
+      def _toList(uc: Option[Cons[A]]): List[A] = {
         uc match {
           case None => Nil
-          case Some(c) => c.head :: tl(c.tail.uncons)
+          case Some(c) => c.head :: _toList(c.tail.uncons)
         }
       }
       if (isEmpty) Nil
-      else tl(uncons)
+      else _toList(uncons)
     }
-    
+
     def take(n: Int): Stream[A] = {
-      def tl(n: Int, uc: Stream[A]): Stream[A] = {
+      def _take(n: Int, uc: Stream[A]): Stream[A] = {
         if (n <= 0) Stream.empty
         else if (uc.isEmpty) Stream.empty
         else uc.uncons match {
           case None => Stream.empty
-          case Some(c) => Stream.cons(c.head, tl(n -1, c.tail))
+          case Some(c) => Stream.cons(c.head, _take(n - 1, c.tail))
         }
       }
       if (isEmpty) Stream.empty
-      else tl(n, this)
+      else _take(n, this)
     }
     def takeWhile(f: A => Boolean): Stream[A] = {
-      def tl(uc: Stream[A]): Stream[A] = {
+      def _takeWhile(uc: Stream[A]): Stream[A] = {
         if (uc.isEmpty) Stream.empty
         else uc.uncons match {
           case None => Stream.empty
           case Some(c) => {
-            if (f(c.head)) Stream.cons(c.head, tl(c.tail))
+            if (f(c.head)) Stream.cons(c.head, _takeWhile(c.tail))
             else Stream.empty
           }
         }
       }
       if (isEmpty) Stream.empty
-      else tl(this)
+      else _takeWhile(this)
+    }
+    // Implement forAll, which checks that all elements in the
+    // match a given predicate. Your implementation should Stream terminate the
+    // traversal as soon as it encounters a non-matching value.
+    def forAll(p: A => Boolean): Boolean = {
+      def _forAll(uc: Stream[A]): Boolean = {
+        if (uc.isEmpty) true
+        else uc.uncons match {
+          case None => true
+          case Some(c) => {
+            if (p(c.head)) _forAll(c.tail)
+            else false
+          }
+        }
+      }
+      if (isEmpty) true
+      else _forAll(this)
     }
   }
   object Stream {
@@ -73,7 +90,7 @@ class Exercises extends FunSuite {
     {
       val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
       val l = s.toList
-      assert(List(1,2,3) === l)
+      assert(List(1, 2, 3) === l)
     }
     {
       val s: Stream[Int] = cons(3, empty)
@@ -116,7 +133,7 @@ class Exercises extends FunSuite {
       assert(Nil === l)
     }
   }
-  
+
   test("Exercise 3 takeWhile") {
     import Stream._
     {
@@ -139,10 +156,38 @@ class Exercises extends FunSuite {
       val l = s.takeWhile((i: Int) => true).toList
       assert(List(1, 2, 3) === l)
     }
-    
-    
-    
-    
+  }
+
+  // Implement forAll, which checks that all elements in the
+  // match a given predicate. Your implementation should Stream terminate the
+  // traversal as soon as it encounters a non-matching value.
+  test("Exercise 4 forAll") {
+    import Stream._
+    {
+      val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+      val re = s.forAll((i: Int) => i > 0)
+      assert(true === re)
+    }
+    {
+      val s: Stream[Int] = empty
+      val re = s.forAll((i: Int) => true)
+      assert(true === re)
+    }
+    {
+      val s: Stream[Int] = empty
+      val re = s.forAll((i: Int) => false)
+      assert(true === re)
+    }
+    {
+      val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+      val re = s.forAll((i: Int) => i > 1)
+      assert(false === re)
+    }
+    {
+      val s: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+      val re = s.forAll((i: Int) => true)
+      assert(true === re)
+    }
   }
 
 }
