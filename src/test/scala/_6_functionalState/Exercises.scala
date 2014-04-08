e55b9e8ce77bde24429736a6d3eb84e1d0bd83a3
wwan <wwagner4@gmail.com>
2014-04-08 18:34:52 +0200

    Started ex 5

--- src/test/scala/_6_functionalState/Exercises.scala
+++ src/test/scala/_6_functionalState/Exercises.scala
@@ -2,21 +2,37 @@ package _6_functionalState
 
 import org.scalatest.FunSuite
 
-trait RNG {
-  def nextInt: (Int, RNG)
-}
+class Exercises_6 extends FunSuite {
 
-object RNG {
-  def simple(seed: Long): RNG = new RNG {
-    def nextInt = {
-      val seed2 = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1)
-      ((seed2 >>> 16).asInstanceOf[Int], simple(seed2))
-    }
+  type Rand[+A] = RNG => (A, RNG)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = {
+    rng =>
+      {
+        val (a, rng2) = s(rng)
+        (f(a), rng2)
+      }
+  }
+  trait RNG {
+    def nextInt: (Int, RNG)
+    val int: Rand[Int] = _.nextInt
+    def unit[A](a: A): Rand[A] = rng => (a, rng)
+    def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+      rng => {
+        val (a, rng2) = s(rng)
+        (f(a), rng2)
+      }
   }
 
-}
+  object RNG {
+    def simple(seed: Long): RNG = new RNG {
+      def nextInt = {
+        val seed2 = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1)
+        ((seed2 >>> 16).asInstanceOf[Int], simple(seed2))
+      }
+    }
 
-class Exercises_6 extends FunSuite {
+  }
 
   test("RNG") {
     val rng1 = RNG.simple(92387923L)
@@ -48,8 +64,7 @@ class Exercises_6 extends FunSuite {
       rng1 = rng2
     })
   }
-  
-  
+
   // EXERCISE 2: Write a function to generate a Double between 0 and 1, not
   // including 1. Note: you can use Int.MaxValue to obtain the maximum positive
   // integer value and you can use x.toDouble to convert an Int, x, to a Double.
@@ -61,7 +76,7 @@ class Exercises_6 extends FunSuite {
     }
     (r.toDouble / Int.MaxValue, r1._2)
   }
-  
+
   test(s"Ex 2") {
     var rng1: RNG = RNG.simple(92387923L)
     (1 to 100) foreach (i => {
@@ -71,20 +86,20 @@ class Exercises_6 extends FunSuite {
       rng1 = rng2
     })
   }
-  
+
   // EXERCISE 4: Write a function to generate a list of random integers
   def ints(cnt: Int)(rng: RNG): (List[Int], RNG) = {
     def _ints(c: Int, rng: RNG): (List[Int], RNG) = {
       if (c <= 0) (Nil, rng)
       else {
         val (rv, rng1) = rng.nextInt
-        val (lv, rng2) = _ints(c -1, rng1) 
+        val (lv, rng2) = _ints(c - 1, rng1)
         (rv :: lv, rng1)
       }
     }
     _ints(cnt, rng)
   }
-  
+
   test(s"Ex 4") {
     var rng1: RNG = RNG.simple(92387923L)
     (1 to 100) foreach (i => {
@@ -95,9 +110,23 @@ class Exercises_6 extends FunSuite {
     })
   }
   
-  
-  
-  
+  test("RNG int") {
+    val rng0 = RNG.simple(203498L)
+    val x = rng0.int(rng0)._1
+    println(s"- RNG int: $x")
+  }
+
+  // EXERCISE 5: Use map to generate an Int between 0 and n, inclusive:
+  def positiveMax(n: Int): Rand[Int] = {
+    val rng0 = RNG.simple(203498L)
+    val rng1 = RNG.simple(203498L) 
+    rng1.int(rng0)(i: Int => i % n)
+  }
+  test(s"Ex 5") {
+    val rng0 = RNG.simple(92387923L)
+    val (x, rng1) = positiveMax(10)(rng0)
+  }
+
 }    
     
 
